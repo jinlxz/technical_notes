b3ae94daf34cd2a439ae44a49009154662e2bbc5
Owen.Liu <flynnhome.liu@gmail.com>
2019-12-06 12:09:42 +0800

    Update k8s_run_busybox.sh

--- software_configuration/k8s_run_busybox.sh
+++ software_configuration/k8s_run_busybox.sh
@@ -1,1 +1,9 @@
-kubectl run kn-generic-17 -n cdqa-shared-services --generator=run-pod/v1 --overrides='{ "apiVersion": "v1", "spec": { "nodeName": "kn-generic-17" } }' --attach=false --image=busybox -i --command --limits='cpu=100m,memory=256Mi' --requests='cpu=100m,memory=256Mi' -- sh -c 'cat'
+#!/bin/bash
+context="cluster1"
+
+kubectl run kn-generic-17 --context $context -n cdqa-shared-services --generator=run-pod/v1 --overrides='{ "apiVersion": "v1", "spec": { "nodeName": "kn-generic-17" } }' --attach=false --image=busybox -i --command --limits='cpu=100m,memory=256Mi' --requests='cpu=100m,memory=256Mi' -- sh -c 'cat'
+kubectl --context $context -n cdqa-shared-services get pod | grep kn-generic | awk '{print $1}' > pods.txt
+
+while read pod; do
+        kubectl --context $context -n cdqa-shared-services exec "$pod" date &
+done < pods.txt
